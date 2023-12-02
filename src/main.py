d9e99a582ed869ceedc0b5929c1af97bec4fed3c
Christoph <91657767+christophlandolt@users.noreply.github.com>
2023-12-02 12:25:59 +0100

    Calling the data handler class from main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from settings import ProjectSettings
 from json_query_engine import QueryEngine
 from json_query_engine import json_value, json_schema
+from data_handler import DataHandler
 if __name__ == '__main__':
     print("Project entry point")
     # Usage
@@ -13,8 +14,14 @@ if __name__ == '__main__':
     print(config.DB_NAME)
     print(config.COLLECTION_NAME)
 
-    query_engine = QueryEngine(config.OPENAI_API_KEY, json_value, json_schema)
-    print(query_engine.get_nl_response("How old is Anna Smith?"))
-    print(query_engine.get_raw_response("How old is Anna Smith?"))
-    
+    #query_engine = QueryEngine(config.OPENAI_API_KEY, json_value, json_schema)
+    #print(query_engine.get_nl_response("How old is Anna Smith?"))
+    #print(query_engine.get_raw_response("How old is Anna Smith?"))
+
+    data_handler = DataHandler(config.DB_SERVER, config.DB_PORT, config.DB_NAME, config.COLLECTION_NAME)
+    places = data_handler.get_places()
+    for id, place in places:
+        print(place)
+
+    data_handler.close_connection()
 
