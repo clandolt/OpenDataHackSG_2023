dd22f80ef2a239ed40afd50298dd9fac934e8c18
Christoph <91657767+christophlandolt@users.noreply.github.com>
2023-12-02 11:51:41 +0100

    Query engine class

--- src/json_query_engine/json_query_engine.py
+++ src/json_query_engine/json_query_engine.py
@@ -1,5 +1,3 @@
-from jsons import json_value, json_schema
-
 import os
 import openai
 from llama_index.indices.service_context import ServiceContext
@@ -7,28 +5,82 @@ from llama_index.llms import OpenAI
 from llama_index.indices.struct_store import JSONQueryEngine
 
 
-os.environ["OPENAI_API_KEY"] = "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"
-openai.api_key = os.environ["OPENAI_API_KEY"]
+class QueryEngine:
+    """
+    This class handles encodes the Text Query Prompt and queries the JSON-File
 
-llm = OpenAI(model="gpt-3.5-turbo")
-service_context = ServiceContext.from_defaults(llm=llm)
-nl_query_engine = JSONQueryEngine(
-    json_value=json_value,
-    json_schema=json_schema,
-    service_context=service_context,
-)
-raw_query_engine = JSONQueryEngine(
-    json_value=json_value,
-    json_schema=json_schema,
-    service_context=service_context,
-    synthesize_response=False,
-)
+    ...
 
-nl_response = nl_query_engine.query(
-    "How old is Anna Smith?",
-)
-raw_response = raw_query_engine.query(
-    "How old is Anna Smith?",
-)
+    Attributes
+    ----------
+    nl_query_engine : JSONQueryEngine
+    raw_query_engine : JSONQueryEngine
 
-print(nl_response)
+    Methods
+    -------
+    get_nl_response(query : str)
+    get_raw_response(query : str)
+
+    """
+    def __init__(self, open_api_key : str, json_value : dict, json_schema : dict):
+        """
+        Parameters
+        ----------
+        open_api_key : str
+            Key to access the OpenAI API
+        json_value : dict
+            A JSON file with values as dict
+        json_schema : dict
+            A JSON file with the schema to query the data
+        """
+        os.environ["OPENAI_API_KEY"] = open_api_key
+        openai.api_key = os.environ["OPENAI_API_KEY"]
+
+        llm = OpenAI(model="gpt-3.5-turbo")
+        service_context = ServiceContext.from_defaults(llm=llm)
+        # a query engine which returns natural language answers
+        self.nl_query_engine = JSONQueryEngine(
+            json_value=json_value,
+            json_schema=json_schema,
+            service_context=service_context,
+        )
+        # a query engine which returns JSON data as answer
+        self.raw_query_engine = JSONQueryEngine(
+            json_value=json_value,
+            json_schema=json_schema,
+            service_context=service_context,
+            synthesize_response=False,
+        )
+
+    def get_nl_response(self, query : str) -> str:
+        """
+        This method queries the JSON-File and returns the answer as natural language text.
+
+        Parameters
+        ----------
+        query : str
+            The query text as string
+
+        Returns
+        -------
+        str
+            The answer to the question as string
+        """
+        return self.nl_query_engine.query(query,)
+
+    def get_raw_response(self, query : str) -> dict:
+        """
+        This method queries the JSON-File and returns the answer as JSON-structured data.
+
+        Parameters
+        ----------
+        query : str
+            The query text as string
+
+        Returns
+        -------
+        dict
+            The answer as JSON answer
+        """
+        return self.raw_query_engine.query(query,)
+
