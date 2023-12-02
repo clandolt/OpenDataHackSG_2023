e00d40621d4427fa27662a91cb64da345b1fa761
Christoph <91657767+christophlandolt@users.noreply.github.com>
2023-12-02 08:51:40 +0100

    Package definition ProjectSettings

--- src/main.py
+++ src/main.py
@@ -1,2 +1,12 @@
+from settings import ProjectSettings
 if __name__ == '__main__':
     print("Project entry point")
+    # Usage
+    config = ProjectSettings.get_instance()
+
+    # Accessing the variables
+    print(config.OPENAI_API_KEY)
+    print(config.DB_SERVER)
+    print(config.DB_PORT)
+    print(config.DB_NAME)
+    print(config.COLLECTION_NAME)
